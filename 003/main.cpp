f3ec46f58528f91489003acb1b454d2d35171b10
naharagu <naharagu@student.42tokyo.jp>
2022-05-14 17:50:49 +0200

    first commit

---
+++ 003/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> vec(n);
+
+	for (int i = 0; i < n; i++)
+		cin >> vec.at(i);
+
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+		sum += vec.at(i);
+
+	cout << sum << endl;
+}
